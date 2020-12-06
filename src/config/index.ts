cea8559c88feca21ae55c554b184b34f99f2fe95
Vladimirs Timofejevs <vladimirs.timofejevs@outlook.com>
2020-12-06 21:19:30 +0000

    APP_ROUTE config added

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,7 @@ const env = process.env.NODE_ENV || 'development'
 const TC_HOST = env === 'production' ? 'topcoder' : 'topcoder-dev'
 
 export const Config = {
+  APP_ROUTE: '/tasks',
   API_URL: `https://api.${TC_HOST}.com/v5`,
   APP_URL: `https://${TC_HOST}.com/challenges`,
   API_V4_URL: `https://api.${TC_HOST}.com/v4`,
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { ListView } from '../screens/list'
+import { Config } from '../config'
 
 // Here, more routes can be added
 const routes = [
   {
     name: 'tasks',
-    url: '/tasks',
+    url: Config.APP_ROUTE,
     component: <ListView />,
     isExact: true,
     home: true
