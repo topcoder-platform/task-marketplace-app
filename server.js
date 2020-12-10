fa134335d80c9283d8b1588d80675245a7e4c6fe
Vladimirs Timofejevs <vladimirs.timofejevs@outlook.com>
2020-12-10 12:53:45 +0000

    update serve path

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 /* global process */
 const express = require('express')
+const fs = require('fs')
+const https = require('https')
 
 const app = express()
 
 app.use(
-  '/tasks',
+  '/task-marketplace',
   express.static('./dist', {
     setHeaders: function setHeaders(res) {
       res.header('Access-Control-Allow-Origin', '*')
@@ -21,6 +23,11 @@ app.get('/', function (req, res) {
   res.send('alive')
 })
 
-const PORT = process.env.PORT || 8080
-app.listen(PORT)
-console.log(`App is hosted on port ${PORT}.`) // eslint-disable-line no-console
+const PORT = process.env.PORT || 8502
+https.createServer({
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert')
+}, app)
+  .listen(PORT, function () {
+    console.log(`App is hosted on port ${PORT}.`) // eslint-disable-line no-console
+  })
