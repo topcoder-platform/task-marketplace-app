4dd0f2c7b8cedcbcad7f7fea81b2fd1bf22b94c4
Vladimirs Timofejevs <vladimirs.timofejevs@outlook.com>
2020-12-10 19:14:07 +0000

    removed https

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 /* global process */
 const express = require('express')
-const fs = require('fs')
-const https = require('https')
 
 const app = express()
 
@@ -23,11 +21,6 @@ app.get('/', function (req, res) {
   res.send('alive')
 })
 
-const PORT = process.env.PORT || 8502
-https.createServer({
-  key: fs.readFileSync('server.key'),
-  cert: fs.readFileSync('server.cert')
-}, app)
-  .listen(PORT, function () {
-    console.log(`App is hosted on port ${PORT}.`) // eslint-disable-line no-console
-  })
+const PORT = process.env.PORT || 3000
+app.listen(PORT)
+console.log(`App is hosted on port ${PORT}.`) // eslint-disable-line no-console
